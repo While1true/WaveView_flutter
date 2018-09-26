3e25b6537c508012d3b0e8f39d146a184dcc046f
While1true <893825883@qq.com>
2018-09-26 18:52:40 +0800

    Add files via upload

---
+++ WaveExample.dart
@@ -0,0 +1,18 @@
+//
+// Created by ckckck on 2018/9/25.
+//
+import 'package:flutter/material.dart';
+import 'package:flutter_jtest/Widgets/WaveWidget.dart';
+
+void main() => runApp(WaveWidget(
+      bgColor: Colors.lightBlue,
+      imageProvider: AssetImage('assets/pic/ic_headimg.png'),
+      imgSize: Size(50.0, 0.0),
+      size: Size(300.0, 500.0),
+    ));
+
+class A {
+  void _a() {
+
+  }
+}
