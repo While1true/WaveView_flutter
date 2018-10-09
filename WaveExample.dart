192b7c37c81db1f5eef731731b774c320ea3591f
While1true <893825883@qq.com>
2018-10-09 11:16:01 +0800

    Update WaveExample.dart

--- WaveExample.dart
+++ WaveExample.dart
@@ -7,7 +7,7 @@ import 'package:flutter_jtest/Widgets/WaveWidget.dart';
 void main() => runApp(WaveWidget(
       bgColor: Colors.lightBlue,
 //       imageProvider: AssetImage('assets/pic/ic_headimg.png'),
-      imgSize: Size(50.0, 0.0),
+//       imgSize: Size(50.0, 0.0),
       size: Size(300.0, 500.0),
     ));
 
